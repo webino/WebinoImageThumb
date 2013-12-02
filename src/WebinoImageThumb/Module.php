a9eeb3427e0c8c7ec8279ffbcdb0eb93b7279d53
Peter Bacinsky <peter@bacinsky.sk>
2013-12-02 13:06:28 +0100

    Moved classmap

--- src/WebinoImageThumb/Module.php
+++ src/WebinoImageThumb/Module.php
@@ -29,11 +29,11 @@ class Module
     {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/../../autoload_classmap.php',
+                __DIR__ . '/autoload_classmap.php',
             ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__,
                 ),
             ),
         );
