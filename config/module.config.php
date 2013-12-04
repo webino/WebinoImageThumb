3764bcf737153f1bd79f084ae2308b88ee9900c1
Peter Bacinsky <peter@bacinsky.sk>
2013-12-04 16:12:11 +0100

    Updated module config, added comments

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,15 @@
 <?php
+/**
+ * Webino (https://github.com/webino/)
+ *
+ * @link      https://github.com/webino/WebinoImageThumb/ for the canonical source repository
+ * @copyright Copyright (c) 2013 Webino, s. r. o. (http://webino.sk/)
+ * @license   New BSD License
+ */
 
+/**
+ * Do not write your custom settings into this file
+ */
 return array(
     'di' => array(
         'definition' => array(
