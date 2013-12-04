109696cf4731fd76433be17c3fed1b21304f9dcf
Peter Bacinsky <peter@bacinsky.sk>
2013-12-04 16:17:00 +0100

    Updated root Module.php, added comments

--- Module.php
+++ Module.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoImageThumb for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     New BSD License
+ */
 
 require __DIR__ . '/src/WebinoImageThumb/Module.php';
